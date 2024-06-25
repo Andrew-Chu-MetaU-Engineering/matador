3b873bc3e897a270025c676287e5a72d256ea1f4
Andrew Chu <andrewchu@meta.com>
2024-06-25 14:00:44 -0700

    search panel [wip]

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,19 @@
-import { Title, Group, Button, Box, ActionIcon } from "@mantine/core";
-import { IconBrandInstagram } from "@tabler/icons-react";
+import {
+  Paper,
+  Title,
+  Group,
+  Button,
+  Box,
+  ActionIcon,
+  TextInput,
+  NumberInput,
+} from "@mantine/core";
+import {
+  IconSearch,
+  IconAdjustments,
+  IconArrowRight,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
 import "./Home.css";
 
 export default function Home() {
@@ -16,7 +30,46 @@ export default function Home() {
           </Group>
         </header>
       </Box>
-      
+      <Paper id="home-body">
+        <Box id="search-panel">
+          <span id="searchbox-span">
+            <TextInput
+              radius="md"
+              size="md"
+              placeholder="Search"
+              leftSection={<IconSearch stroke={1.5} />}
+              rightSection={
+                <ActionIcon radius="sm" variant="light">
+                  <IconArrowRight stroke={1.5} />
+                </ActionIcon>
+              }
+            />
+            <ActionIcon variant="filled">
+              <IconAdjustments stroke={1.5} />
+            </ActionIcon>
+          </span>
+          <Box id="search-filters">
+            <NumberInput
+              label="Transit fare"
+              prefix="< $"
+              allowNegative={false}
+              allowDecimal={false}
+              thousandSeparator=","
+            />
+            <NumberInput
+              label="Transit duration"
+              prefix="< "
+              suffix=" min"
+              allowNegative={false}
+              allowDecimal={false}
+              thousandSeparator=","
+            />
+          </Box>
+        </Box>
+        <Box id="map-area">
+          <h1>Map Area</h1>
+        </Box>
+      </Paper>
       <Box>
         <footer id="footer">
           <Group id="footer-spacing-group">
