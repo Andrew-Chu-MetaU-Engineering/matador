9017dbd2a32e70ab8f14d9353a0a17d71aa9d906
Andrew Chu <andrewchu@meta.com>
2024-06-25 10:42:34 -0700

    footer

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,18 +1,32 @@
-import { Title, Group, Button, Box } from "@mantine/core";
+import { Title, Group, Button, Box, ActionIcon } from "@mantine/core";
+import { IconBrandInstagram } from "@tabler/icons-react";
 import "./Home.css";
 
 export default function Home() {
   return (
-    <Box pb={120}>
-      <header id="header">
-        <Group id="header-spacing-group">
-          <Title order={1}>Matador</Title>
-          <Group>
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
+    <>
+      <Box>
+        <header id="header">
+          <Group id="header-spacing-group">
+            <Title order={1}>Matador</Title>
+            <Group>
+              <Button variant="default">Log in</Button>
+              <Button>Sign up</Button>
+            </Group>
           </Group>
-        </Group>
-      </header>
-    </Box>
+        </header>
+      </Box>
+      
+      <Box>
+        <footer id="footer">
+          <Group id="footer-spacing-group">
+            <Title order={2}>Matador</Title>
+            <ActionIcon size="lg" color="gray" variant="subtle">
+              <IconBrandInstagram stroke={1.5} />
+            </ActionIcon>
+          </Group>
+        </footer>
+      </Box>
+    </>
   );
 }
