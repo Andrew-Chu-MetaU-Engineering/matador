07e429442d468fcc99fd918057bd5cfe942f9e5d
Andrew Chu <andrewchu@meta.com>
2024-06-28 13:59:47 -0700

    rename utils

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const utils = require("./utils");
+const utils = require("./FetchResultsUtils");
 require("dotenv").config();
 
 const app = express();
