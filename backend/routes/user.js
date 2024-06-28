e71f74b19d344b28f0c9c92e64e59323ddd70cb5
Andrew Chu <andrewchu@meta.com>
2024-06-28 11:45:06 -0700

    express + db integration

---
+++ backend/routes/user.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+router.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
+});
+
+router.get("/:id", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(req.params.id) },
+  });
+  res.status(200).json(user);
+});
+
+router.post("/", async (req, res) => {
+  const newUser = await prisma.user.create();
+  res.status(201).json(newUser);
+});
+
+router.delete("/:id", async (req, res) => {
+  const deletedUser = await prisma.user.delete({
+    where: { id: parseInt(req.params.id) },
+  });
+  res.status(204).json(deletedUser);
+});
+
+router.put("/:id/hobby", async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: parseInt(req.params.id),
+    },
+    data: {
+      hobbies: {
+        push: req.body.hobby,
+      },
+    },
+  });
+  res.status(204).json(user);
+});
+
+router.put("/:id/cuisine", async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: parseInt(req.params.id),
+    },
+    data: {
+      cuisines: {
+        push: req.body.cuisine,
+      },
+    },
+  });
+  res.status(204).json(user);
+});
+
+module.exports = router;
