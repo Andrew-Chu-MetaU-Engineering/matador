74951c1ae00659a30391ab16e8e2653597d3503c
Andrew Chu <andrewchu@meta.com>
2024-07-08 11:43:17 -0700

    strict preference checkboxes

--- frontend/src/pages/home/Search.jsx
+++ frontend/src/pages/home/Search.jsx
@@ -1,6 +1,14 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
-import { Paper, Box, ActionIcon, TextInput, NumberInput } from "@mantine/core";
+import {
+  Paper,
+  Group,
+  Box,
+  ActionIcon,
+  TextInput,
+  NumberInput,
+  Checkbox,
+} from "@mantine/core";
 
 import {
   IconMapSearch,
@@ -52,25 +60,31 @@ export default function Search({ form, handleSearch }) {
         </span>
         {showFilters && (
           <Box id="search-filters">
-            <NumberInput
-              key={form.key("fare")}
-              {...form.getInputProps("fare")}
-              label="Transit fare"
-              prefix="< $"
-              allowNegative={false}
-              allowDecimal={false}
-              thousandSeparator=","
-            />
-            <NumberInput
-              key={form.key("duration")}
-              {...form.getInputProps("duration")}
-              label="Transit duration"
-              prefix="< "
-              suffix=" min"
-              allowNegative={false}
-              allowDecimal={false}
-              thousandSeparator=","
-            />
+            <Group>
+              <NumberInput
+                key={form.key("fare")}
+                {...form.getInputProps("fare")}
+                label="Transit fare"
+                prefix="< $"
+                allowNegative={false}
+                allowDecimal={false}
+                thousandSeparator=","
+              />
+              <Checkbox size="xs" label="No higher fares" />
+            </Group>
+            <Group>
+              <NumberInput
+                key={form.key("duration")}
+                {...form.getInputProps("duration")}
+                label="Transit duration"
+                prefix="< "
+                suffix=" min"
+                allowNegative={false}
+                allowDecimal={false}
+                thousandSeparator=","
+              />
+              <Checkbox size="xs" label="No longer durations" />
+            </Group>
           </Box>
         )}
       </form>
