26d0ddc4773521374b13d74401ee55a4943bc676
Andrew Chu <andrewchu@meta.com>
2024-07-09 14:11:47 -0700

    search result details

--- frontend/src/pages/home/SearchResult.jsx
+++ frontend/src/pages/home/SearchResult.jsx
@@ -1,13 +1,36 @@
 import PropTypes from "prop-types";
-import { Card, Text } from "@mantine/core";
+import { Card, Text, Rating, Group } from "@mantine/core";
+import "./SearchResult.css";
 
 export default function SearchResult({ option }) {
+  const { place, extracted } = option;
+  const durationMinutes = Math.round(extracted.duration / 60); // extracted.duration is in seconds
   return (
     <Card shadow="xs" padding="md" radius="md" mb="xs">
-      <Text size="lg">{option.place.displayName.text}</Text>
-      <Text size="md" c="dimmed" truncate="end">
-        1h 26m &middot; $6.25
-      </Text>
+      <Text size="lg">{place.displayName.text}</Text>
+      <div id="place-properties">
+        <Text size="md" c="dimmed" truncate="end">
+          {durationMinutes >= 60 && (
+            <span>{Math.trunc(durationMinutes / 60)}h </span>
+          )}
+          {durationMinutes % 60}m &middot; ${extracted.fare.toFixed(2)}
+        </Text>
+        <div id="price-and-rating-wrapper">
+          {extracted.priceLevel > 0 && (
+            <Text size="sm" c="dimmed" truncate="end">
+              {"$".repeat(extracted.priceLevel)}
+            </Text>
+          )}
+          {place.rating && (
+            <Group gap="xs">
+              <Rating value={place.rating} fractions={2} size="xs" readOnly />
+              <Text size="sm" c="dimmed" truncate="end">
+                {place.rating.toFixed(1)}
+              </Text>
+            </Group>
+          )}
+        </div>
+      </div>
     </Card>
   );
 }
