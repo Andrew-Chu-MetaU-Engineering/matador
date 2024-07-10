fd1fe531cac0d6867e212f27c7c8f1f52fe89663
Andrew Chu <andrewchu@meta.com>
2024-07-10 10:03:51 -0700

    link profile page to backend apis

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,11 +1,66 @@
-import { TextInput, Paper, ActionIcon } from "@mantine/core";
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { TextInput, Paper, ActionIcon, List } from "@mantine/core";
 import { IconPlus } from "@tabler/icons-react";
 import "./Profile.css";
 
-function Profile() {
+function Profile({ userId }) {
+  const { VITE_EXPRESS_API } = import.meta.env;
+  const [interests, setInterests] = useState([]);
+  const [interestInput, setInterestInput] = useState("");
+
+  useEffect(() => {
+    fetchInterests();
+  }, [userId]);
+
+  async function fetchInterests() {
+    if (userId == null) return;
+    try {
+      const response = await fetch(
+        new URL(`user/${userId}/interests`, VITE_EXPRESS_API)
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error. Status ${response.status}`);
+      }
+      const data = await response.json();
+      setInterests(data.interests);
+    } catch (error) {
+      console.error("Error fetching interests:", error);
+    }
+  }
+
+  async function addInterest() {
+    if (interests.includes(interestInput)) return;
+    try {
+      const response = await fetch(
+        new URL(`user/${userId}/interests`, VITE_EXPRESS_API),
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            interests: [...interests, interestInput],
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error. Status ${response.status}`);
+      }
+
+      fetchInterests();
+      setInterestInput("");
+    } catch (error) {
+      console.error("Error adding interest:", error);
+    }
+  }
+
   return (
     <Paper id="profile-body">
       <TextInput
+        value={interestInput}
+        onChange={(e) => setInterestInput(e.currentTarget.value)}
         id="profile-text-input"
         radius="xl"
         size="xl"
@@ -15,13 +70,29 @@ function Profile() {
           <img id="profile-input-bull-icon" src="noun-project-bull.svg" />
         }
         rightSection={
-          <ActionIcon size="lg" radius="xl" variant="filled">
+          <ActionIcon
+            onClick={addInterest}
+            size="lg"
+            radius="xl"
+            variant="filled"
+          >
             <IconPlus stroke={3} />
           </ActionIcon>
         }
       />
+
+      <List spacing="xs" size="sm" center mt="md">
+        {interests &&
+          interests?.map((interest) => (
+            <List.Item key={interest}>{interest}</List.Item>
+          ))}
+      </List>
     </Paper>
   );
 }
 
 export default Profile;
+
+Profile.propTypes = {
+  userId: PropTypes.string,
+};
