5ecf3574193c15f76b2ed827e01c0b2cead3ef97
Andrew Chu <andrewchu@meta.com>
2024-07-12 15:55:28 -0700

    add cost prediction

--- backend/IsographUtils.js
+++ backend/IsographUtils.js
@@ -20,35 +20,58 @@ function toRadians(degrees) {
 }
 
 async function fetchCosts(origin, samplePoints, costType, departureTime) {
-  const routesData = await fetchResultsUtils.fetchRouteMatrix(
-    origin,
-    samplePoints,
-    departureTime,
-    true
-  );
+  try {
+    const routesData = await fetchResultsUtils.fetchRouteMatrix(
+      origin,
+      samplePoints,
+      departureTime,
+      true
+    );
 
-  if (costType === "duration") {
-    parseCostFunction = fetchResultsUtils.parseDuration;
-  } else if (costType === "fare") {
-    parseCostFunction = fetchResultsUtils.calculateFare;
-  } else {
-    console.error("Cost type not supported.");
+    if (costType === "duration") {
+      parseCostFunction = fetchResultsUtils.parseDuration;
+    } else if (costType === "fare") {
+      parseCostFunction = fetchResultsUtils.calculateFare;
+    } else {
+      console.error("Cost type not supported.");
+    }
+    return routesData.map((route) => parseCostFunction(route));
+  } catch (error) {
+    console.error(error.message);
   }
-
-  return routesData.map((route, i) => [
-    samplePoints[i],
-    parseCostFunction(route),
-  ]);
 }
 
-function get3DPoints(sampleInfo) {
-  return sampleInfo
-    .map((sample) => Array.from(sample.points).map(([k, v]) => [k[0], k[1], v]))
-    .flat();
+function predictCostDistances(costs, distances, costIntervals) {
+  let predictedDistances = [];
+  let currentCostIntervalIndex = 0;
+  let currentCostInterval = costIntervals[currentCostIntervalIndex];
+
+  for (let i = 0; i < costs.length; i++) {
+    const cost = costs[i];
+    const distance = distances[i];
+    if (cost >= currentCostInterval) {
+      // if prediction point is between samples, linear interpolate
+      const prevCost = i === 0 ? 0 : costs[i - 1];
+      const prevDist = i === 0 ? 0 : distances[i - 1];
+      const slope = (distance - prevDist) / (cost - prevCost);
+
+      predictedDistances.push([
+        currentCostInterval,
+        slope * (currentCostInterval - cost) + distance, // point-slope form
+      ]);
+      currentCostInterval = costIntervals[++currentCostIntervalIndex];
+    } else {
+      if (i === costs.length - 1) {
+        // if prediction interval is after all samples, final sample distance is within prediction cost
+        predictedDistances.push([currentCostInterval, distance]);
+      }
+    }
+  }
+  return predictedDistances;
 }
 
 module.exports = {
   findCoordinate,
   fetchCosts,
-  get3DPoints,
+  predictCostDistances,
 };
