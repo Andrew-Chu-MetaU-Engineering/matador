3f582b69d00ec72c8073ff3bf2dea161ee1e39ab
Andrew Chu <andrewchu@meta.com>
2024-07-19 16:37:24 -0700

    RouteDetail frontend layout

--- frontend/src/pages/routedetail/RouteDetail.jsx
+++ frontend/src/pages/routedetail/RouteDetail.jsx
@@ -1,5 +1,13 @@
-function RouteDetail() {
-  return <div>RouteDetail</div>;
-}
+import { Box, Paper } from "@mantine/core";
+import "./RouteDetail.css";
 
-export default RouteDetail;
+export default function RouteDetail() {
+  return (
+    <Paper>
+      <Paper id="route-detail-body">
+        <Box id="route-map-area"></Box>
+        <Paper id="detail-panel" shadow="xs" radius="md"></Paper>
+      </Paper>
+    </Paper>
+  );
+}
