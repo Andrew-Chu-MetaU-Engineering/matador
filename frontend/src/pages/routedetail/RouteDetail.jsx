ddc1abcfd029ae3e66757721de95cd8b1dd2466f
Andrew Chu <andrewchu@meta.com>
2024-07-22 09:48:15 -0700

    route detail component

--- frontend/src/pages/routedetail/RouteDetail.jsx
+++ frontend/src/pages/routedetail/RouteDetail.jsx
@@ -1,12 +1,43 @@
-import { Box, Paper } from "@mantine/core";
+import { useLocation } from "react-router-dom";
+import { Box, Paper, ScrollArea, Text } from "@mantine/core";
+import StepInfoPane from "./StepInfoPane";
+import TransitMap from "../../components/TransitMap";
+import HomeButton from "../../components/HomeButton";
 import "./RouteDetail.css";
 
 export default function RouteDetail() {
+  const {
+    place,
+    route: { viewport, polyline, legs },
+  } = useLocation().state.option;
+
   return (
     <Paper>
+      <HomeButton />
       <Paper id="route-detail-body">
-        <Box id="route-map-area"></Box>
-        <Paper id="detail-panel" shadow="xs" radius="md"></Paper>
+        <Box id="route-map-area">
+          <TransitMap
+            encodedPath={polyline.encodedPolyline}
+            focusBounds={{
+              north: viewport.high.latitude,
+              east: viewport.high.longitude,
+              south: viewport.low.latitude,
+              west: viewport.low.longitude,
+            }}
+          />
+        </Box>
+        <Paper id="detail-panel" shadow="xs" radius="md">
+          <Text size="lg" fw={800} ta="center" m="sm">
+            Route to {place.displayName.text}
+          </Text>
+          <ScrollArea>
+            {legs?.map((leg) => {
+              return leg?.steps.map((step, i) => (
+                <StepInfoPane key={i} step={step} />
+              ));
+            })}
+          </ScrollArea>
+        </Paper>
       </Paper>
     </Paper>
   );
