c58693d856a659d934b5674dab1580a36126392c
Andrew Chu <andrewchu@meta.com>
2024-07-23 09:22:32 -0700

    create search pane

---
+++ frontend/src/pages/home/search/SearchPane.jsx
@@ -0,0 +1,53 @@
+import PropTypes from "prop-types";
+import { ScrollArea, Loader } from "@mantine/core";
+import Search from "./Search";
+import SearchResult from "./SearchResult";
+import "./SearchPane.css";
+
+export default function SearchPane({
+  form,
+  handleFormSubmit,
+  options,
+  isResultsLoading,
+  noResultsFound,
+  activeOption,
+  setActiveOption,
+  setIsRouteDetailDisplayed,
+}) {
+  return (
+    <>
+      <Search form={form} handleFormSubmit={handleFormSubmit} />
+      {isResultsLoading && (
+        <div id="results-loading-wrapper">
+          <Loader color="blue" size="xl" type="dots" />
+        </div>
+      )}
+      <ScrollArea id="results-scrollarea">
+        {options?.length > 0 &&
+          options.map((option) => (
+            <SearchResult
+              key={option.place.id}
+              option={option}
+              activeOption={activeOption}
+              setActiveOption={setActiveOption}
+              setIsRouteDetailDisplayed={setIsRouteDetailDisplayed}
+            />
+          ))}
+        {noResultsFound && (
+          <p>No results found. Please try a different search.</p>
+        )}
+      </ScrollArea>
+    </>
+  );
+}
+
+SearchPane.propTypes = {
+  form: PropTypes.object.isRequired,
+  handleFormSubmit: PropTypes.func.isRequired,
+  options: PropTypes.array,
+  isResultsLoading: PropTypes.bool.isRequired,
+  noResultsFound: PropTypes.bool.isRequired,
+  activeOption: PropTypes.object,
+  setActiveOption: PropTypes.func.isRequired,
+  setIsRouteDetailDisplayed: PropTypes.func.isRequired,
+};
